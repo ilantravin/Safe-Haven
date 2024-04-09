90f4f68ff75b5d98dba31ced75c92546336abcce
ilantravin <nitzast@ac.sce.ac.il>
2024-04-09 22:37:12 +0300

    creating the project and adding all the paths and all the command needed

--- safehaven/wsgi.py
+++ safehaven/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for SafeHaven project.
+WSGI config for safehaven project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SafeHaven.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safehaven.settings')
 
 application = get_wsgi_application()
